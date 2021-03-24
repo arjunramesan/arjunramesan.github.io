c08423e6d8066ea03c0033bc32bce4924141b319
Arjun Ramesan <arjunramesan69@gmail.com>
2021-03-24 23:09:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-setTimeout(function(){ window.location.href="http://arjunramesan.us-east-1.elasticbeanstalk.com/" }, 2800);
+setTimeout(function(){ window.location.href="http://ec2-3-86-151-199.compute-1.amazonaws.com/" }, 2800);
